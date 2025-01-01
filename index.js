e577998b45ffb2227ae26d49ea08c2d894e982ba
otavio-castro <ocastro@sga.pucminas.br>
2025-01-01 18:21:16 -0300

    Melhorias da API

--- index.js
+++ index.js
@@ -1,14 +1,25 @@
 const http = require('http');
+const url = require('url')
 
 http.createServer((request, response) => {
-    console.log(request.method);
+    const parsedURL = url.parse(request.url, true);
+    const pathName = parsedURL.pathname
+    const queryParams = parsedURL.query;
 
-    if (request.method === 'POST') {
-        response.write('Hello Word, Otavio!');
+    if (pathName === '/favicon.ico') {
+        response.writeHead(404);
+        response.end();
+        return;
+    }
+
+    console.log(queryParams);
+
+    if (request.method === 'GET') {
+        const soma = Number(queryParams.num) + Number(queryParams.num2);
+
+        response.writeHead(200, { 'content-type': 'text/plain; charset=utf-8' });
+        response.write(`Resultado da Soma é: ${soma}`);
         response.end();
     }
-    else if (request.method === 'GET') {
-        response.write('Metodo Get');
-        response.end();
-    }
+
 }).listen(3000)
