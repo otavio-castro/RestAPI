7aaa7a96ef3e3a9319bba0866aa1b7a5b0a74b59
otavio-castro <ocastro@sga.pucminas.br>
2025-01-02 18:54:57 -0300

    Teste de Branch Master

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const http = require('http');
-const url = require('url')
+const url = require('url');
 
 http.createServer((request, response) => {
     const parsedURL = url.parse(request.url, true);
