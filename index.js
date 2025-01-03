3de0744930e525830e565ac46ab8df4cfa424c92
otavio-castro <ocastro@sga.pucminas.br>
2025-01-02 21:40:04 -0300

    EXPRESS [GET, POST]

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
-const aa = 1
+const express = require('express');
+
+const app = express();
+const porta = 3000;
+
+app.get('/', (req, res) => {
+    const soma = Number(req.query.num1) + Number(req.query.num2);
+
+    res.send(`Olá Otávio! <br> Soma dos Parâmetros:${soma}`);
+});
+
+app.post('/', (req, res) => {
+    const usuario = {
+        nome: 'Otavio',
+        idade: 19,
+    }
+
+    res.json(usuario);
+});
+
+app.listen(porta, () => {
+    console.log(`Servidor Rodando na porta ${porta}`);
+})
+
+/*
+SEM EXPRESS
+
+const http = require('http');
+
+http.createServer((req, res) => {
+
+    res.writeHead(200, { "content-type": 'text/plain; charset=utf-8' });
+
+    res.write("Olá Otávio, Sem Express");
+    res.end()
+
+}).listen(9090);
+*/
