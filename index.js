a269d8172e2a0a7c4d55404ba380c6e7e5a15f07
otavio-castro <ocastro@sga.pucminas.br>
2025-01-03 23:28:42 -0300

    Exercicios Iniciais

--- index.js
+++ index.js
@@ -1,47 +1,47 @@
 const express = require('express');
-
 const app = express();
 const porta = 3000;
 
+//Funções Auxiliares
 
-app.get('/', (req, res) => {
+function verificaPar(num) {
+    return (num % 2 == 0);
+}
+
+
+//Rotas HTTP
+
+app.get('/soma', (req, res) => {
     const soma = Number(req.query.num1) + Number(req.query.num2);
 
-    res.send(`Soma dos Parâmetros: ${soma}`);
+    res.send(`Soma: ${soma}`);
 });
 
-app.get('/api/:id', (req, res) => {
-    const id = req.params.id;
 
-    res.send(`Hello Word! ${id}`);
+app.get('/sub', (req, res) => {
+    const num1 = Number(req.query.num1);
+    const num2 = Number(req.query.num2);
+
+    const sub = num1 - num2;
+
+    res.send(`${num1} - ${num2} = ${sub}`)
 });
+
+app.get('/par', (req, res) => {
+    const num = Number(req.query.num);
+
+    if (verificaPar(num))
+        res.send(`${num} é Par`)
+    else
+        res.send(`${num} é Ímpar`)
+
+});
+
+app.get('/calculadora/:sinal', (req, res) => {
+
+})
+
 
 app.listen(porta, () => {
     console.log(`Servidor Rodando na porta ${porta}`);
-})
-
-/*
-app.post('/', (req, res) => {
-    const usuario = {
-        nome: 'Otavio',
-        idade: 19,
-    }
-
-    res.json(usuario);
 });
-*/
-
-/*
-SEM EXPRESS
-
-const http = require('http');
-
-http.createServer((req, res) => {
-
-    res.writeHead(200, { "content-type": 'text/plain; charset=utf-8' });
-
-    res.write("Olá Otávio, Sem Express");
-    res.end()
-
-}).listen(9090);
-*/
